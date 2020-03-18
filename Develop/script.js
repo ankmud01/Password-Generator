e8202440d6e772dd463a5785800bbc53d819c88c
Rebel Soul <mudvari.ankit@gmail.com>
2020-03-18 00:19:31 -0400

    Day 3 on password generator working on creating random letters.

--- Develop/script.js
+++ Develop/script.js
@@ -1,117 +1,65 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
 //variables
-var length;
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "01234567689";
-var symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var lowerCase = randomGenerator("abcdefghijklmnopqrstuvwxyz");
+console.log(lowerCase);
+var upperCase = randomGenerator("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+var numbers = randomGenerator("01234567689");
+var symbols = randomGenerator("!#$%&'()*+,-./:;<=>?@[\]^_`{|}~");
 
+//Prompt and confirm user input 
 var passwordLength = prompt("Please enter a Number betweeen 8 and 128");
 if (passwordLength >= 8 && passwordLength <= 128) {
   console.log(passwordLength);
-  var chooseLowerCase = confirm("Do you want to include LOWERCASE in your password?")
-  console.log(chooseLowerCase);
-  var chooseUpperCase = confirm("Do you want to include UPPERCASE in your password?")
-  console.log(chooseUpperCase);
-  var chooseNumber = confirm("Do you want to include NUMBERS in your password?")
-  console.log(chooseNumber);
-  var chooseSymbols = confirm("Do you want to include SYMBOLS in your password?")
-  console.log(chooseSymbols);
+  var chooseLower = confirm("Do you want to include LOWERCASE in your password?")
+  console.log(chooseLower);
+  var chooseUpper = confirm("Do you want to include UPPERCASE in your password?")
+  console.log(chooseUpper);
+  var chooseNum = confirm("Do you want to include NUMBERS in your password?")
+  console.log(chooseNum);
+  var chooseSymbol = confirm("Do you want to include SYMBOLS in your password?")
+  console.log(chooseSymbol);
 } else {
   alert("You must select a number betweeen 8 and 128 to continue creating password..")
 }
 
-//Function for generating random variables
-function randomLowerLetter() {
-  return lowerCase.split('')[(Math.floor(Math.random() * lowerCase.length))]
-};
-console.log(randomLowerLetter())
-function randomUpperLetter() {
-  return upperCase.split('')[(Math.floor(Math.random() * upperCase.length))]
-};
-console.log(randomUpperLetter());
-function randomNumbers() {
-  return numbers.split('')[(Math.floor(Math.random() * numbers.length))]
-};
-console.log(randomNumbers());
-function randomSymbols() {
-  return symbols.split('')[(Math.floor(Math.random() * symbols.length))]
-};
-console.log(randomSymbols());
+//Function to generate an array of random numbers
 
-password.innerText = generatePassword(chooseLowerCase, chooseUpperCase, chooseNumber, chooseSymbols, passwordLength);
-
-//Creating the generatePassword Function
-var randomLetterFunction = {
-  lowerLetter: randomLowerLetter,
-  upperLetter: randomUpperLetter,
-  symbolsValue: randomSymbols,
-  numbersValue: randomNumbers
-};
-
-
-function generatePassword(chooseLowerCase, chooseUpperCase, chooseNumber, chooseSymbols, passwordLength){
-  //password variable
-  //filter out unchecked items
-  //loop over length & call generator function
-  //add final password to password variable
-
-  var password = '';
-  requiredFeilds = chooseUpperCase + chooseLowerCase + chooseSymbols + chooseNumber
-  console.log ( "required Feilds: ", requiredFeilds);
-
-  var charfeildsArr = [{ chooseLowerCase }, { chooseUpperCase }, { chooseNumber }, { chooseSymbols }].filter
-  (
-    item => Object.values(item)[0]
-  );
-  console.log ( "Character Feilds: ", charfeildsArr);
-
-  if(requiredFeilds === 0){
-    return '';
-  }
-
-  for (let i=0; i<chooseNumber; i+= requiredFeilds){
-    charfeildsArr.forEach(type => {
-      var randomFunction = Object.keys(type)[0];
-      console.log('random Function: ', randomFunction);
-
-      generatePassword += randomLetterFunction[randomFunction()];
-    });
-  }
-
-  console.log (password);
+function randomGenerator(String){
+  var letterarr = []
+  var letter = (String.split(''))
+  console.log("my split letters: " + letter);
+  letterarr.push(letter)
+  console.log("my array letters: " + letterarr);
+  return letterarr;
 }
 
+//Generate Password function
+var password = generatePassword(passwordLength, chooseLower, chooseUpper, chooseNum, chooseSymbol);
+function generatePassword(passwordLength, chooseUpper, chooseNum, chooseSymbol){
+  var combinedLetters = lowerCase
+  if (chooseUpper) combinedLetters = combinedLetters.concat(upperCase)
+  console.log (combinedLetters);
+  if (chooseNum) combinedLetters = combinedLetters.concat(numbers)
+  console.log (combinedLetters);
+  if (chooseSymbol) combinedLetters = combinedLetters.concat(symbols)
+  console.log (combinedLetters);
 
+  var randomValues = [];
+  for(var i=0; i<passwordLength; i++){
+    var value = combinedLetters[Math.floor(Math.random()* combinedLetters.length)]
+    randomValues.push(value)
+  }
+  return randomValues.join('')
+}
 
-
-
-
-
-
-
-// var passwordGen = [];
-// function generatePassword(passwordLength, chooseLowerCase, chooseUpperCase, chooseNumber, chooseSymbols) {
-//   let passwordCharacter = randomLowerLetter()
-//   if (chooseUpperCase) passwordCharacter = passwordCharacter.concat(randomLowerLetter())
-//   if (chooseSymbols) passwordCharacter = passwordCharacter.concat(randomSymbols())
-//   if (chooseNumber) passwordCharacter = passwordCharacter.concat(randomNumbers())
-
-//   for (let i = 0; i <= passwordLength; i++) {
-//     var passwordGen = passwordCharacter[Math.floor(Math.random() * passwordCharacter.length)]
-//   }
-//   return passwordGen.join('')
-// }
-// console.log(passwordGen);
-// Write password to the #password input
+//Write password to the #password input
 function writePassword() {
-  var password = generatePassword(passwordLength, chooseLowerCase, chooseUpperCase, chooseNumber, chooseSymbols);
+  var password = generatePassword(passwordLength, chooseLower, chooseUpper, chooseNum, chooseSymbol);
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.innerHTML = password;
 
 }
 
