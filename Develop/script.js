bd675c70e36dbbc12df429c7a37a8d100e3428c7
Rebel Soul <mudvari.ankit@gmail.com>
2020-03-19 15:08:15 -0400

    Complete most of the dev work for assignment Additional popup work left

--- Develop/script.js
+++ Develop/script.js
@@ -1,67 +1,86 @@
-// Assignment Code
+//Declaring Variables
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "01234567689";
+var symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var password,
+  randomCharacter,
+  passwordLength,
+  chooseLower,
+  chooseNum,
+  chooseUpper,
+  chooseSymbol
 
-//variables
-var lowerCase = randomGenerator("abcdefghijklmnopqrstuvwxyz");
-console.log(lowerCase);
-var upperCase = randomGenerator("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-var numbers = randomGenerator("01234567689");
-var symbols = randomGenerator("!#$%&'()*+,-./:;<=>?@[\]^_`{|}~");
-
-//Prompt and confirm user input 
-var passwordLength = prompt("Please enter a Number betweeen 8 and 128");
+//Prompt and Confirm user Input
+passwordLength = prompt("Please enter a Number betweeen 8 and 128");
 if (passwordLength >= 8 && passwordLength <= 128) {
   console.log(passwordLength);
-  var chooseLower = confirm("Do you want to include LOWERCASE in your password?")
+  chooseLower = confirm("Do you want to include LOWERCASE in your password?")
   console.log(chooseLower);
-  var chooseUpper = confirm("Do you want to include UPPERCASE in your password?")
+  chooseUpper = confirm("Do you want to include UPPERCASE in your password?")
   console.log(chooseUpper);
-  var chooseNum = confirm("Do you want to include NUMBERS in your password?")
+  chooseNum = confirm("Do you want to include NUMBERS in your password?")
   console.log(chooseNum);
-  var chooseSymbol = confirm("Do you want to include SYMBOLS in your password?")
+  chooseSymbol = confirm("Do you want to include SYMBOLS in your password?")
   console.log(chooseSymbol);
-} else {
-  alert("You must select a number betweeen 8 and 128 to continue creating password..")
-}
-
-//Function to generate an array of random numbers
-
-function randomGenerator(String){
-  var letterarr = []
-  var letter = (String.split(''))
-  console.log("my split letters: " + letter);
-  letterarr.push(letter)
-  console.log("my array letters: " + letterarr);
-  return letterarr;
-}
-
-//Generate Password function
-var password = generatePassword(passwordLength, chooseLower, chooseUpper, chooseNum, chooseSymbol);
-function generatePassword(passwordLength, chooseUpper, chooseNum, chooseSymbol){
-  var combinedLetters = lowerCase
-  if (chooseUpper) combinedLetters = combinedLetters.concat(upperCase)
-  console.log (combinedLetters);
-  if (chooseNum) combinedLetters = combinedLetters.concat(numbers)
-  console.log (combinedLetters);
-  if (chooseSymbol) combinedLetters = combinedLetters.concat(symbols)
-  console.log (combinedLetters);
-
-  var randomValues = [];
-  for(var i=0; i<passwordLength; i++){
-    var value = combinedLetters[Math.floor(Math.random()* combinedLetters.length)]
-    randomValues.push(value)
+  if (chooseLower === false && chooseUpper === false && chooseNum === false
+    && chooseSymbol === false) {
+    alert("You must select atleat one input value to generate password ..");
+  } else {
+    alert("Please click on GENERATE PASSWORD button to generate a new password")
   }
-  return randomValues.join('')
+} else {
+  alert("You must select a number betweeen 8 and 128 to continue creating password..");
+}
+
+//Function to generate an array of random character type
+function generatePassword() {
+  password = "";
+  randomCharType
+    = "";
+  if (chooseLower === true) {
+    randomCharType
+      += lowerCase;
+    //This means randomCharType = randomCharType + lowerCase 
+  }
+  if (chooseUpper === true) {
+    randomCharType
+      += upperCase;
+  }
+  if (chooseNum === true) {
+    randomCharType
+      += numbers;
+  }
+  if (chooseSymbol === true) {
+    randomCharType
+      += symbols;
+  }
+  for (var i = 0; i < passwordLength; i++) {
+    password += randomCharType
+      .charAt(Math.floor(Math.random() * randomCharType
+        .length));
+    /*
+    This will select a random character from randomCharType
+    variable, loops through all choices and += keeps adding to the password 
+    untill the length is met..
+    */
+  }
+  // This alert will display user their new password everytime Generate Password is clicked
+  /*
+  alert("Your New Secure Password is: " + password);
+  */
 }
 
 //Write password to the #password input
 function writePassword() {
-  var password = generatePassword(passwordLength, chooseLower, chooseUpper, chooseNum, chooseSymbol);
-  var passwordText = document.querySelector("#password");
-
+  generatePassword();
+  // var passwordText = document.querySelector("#password"); THIS IS REPEAT FOR ME
   passwordText.innerHTML = password;
+  console.log(password);
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
